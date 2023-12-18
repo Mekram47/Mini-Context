586d1f9e1b2765408a98126b9b488069fb1a306f
MD EKRAM <129666126+Mekram47@users.noreply.github.com>
2023-12-18 23:25:43 +0530

    Add files via upload

---
+++ src/context/UserContextProvider.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+
+import userContext from './UserContext'
+
+const userContextProvider=({children})=>{
+    const [user,setUser]= useState(null)
+    return(
+        <userContext.Provider value={{user,setUser}}>
+            {children}
+
+        </userContext.Provider>
+    )
+}
+
+export default userContextProvider
